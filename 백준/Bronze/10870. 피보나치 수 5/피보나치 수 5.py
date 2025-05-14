0666d216f5123045523cb202d9153183b1046789
오가네손 <105881036+oganessone718@users.noreply.github.com>
2025-05-14 16:10:21 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -2,11 +2,15 @@ input_num, = map(int, input().split())
 
 fibo_list = [0,1,1]
 
-def fibo(n):
-    if len(fibo_list)>n:
-        return fibo_list[n]
-    else:
-        fibo_list.append(fibo(n-1)+fibo(n-2))
-        return fibo_list[n]
-    
-print(fibo(input_num))
+# 메모이제이션인데 굳이 재귀 안 써도... bottom up으로
+# def fibo(n):
+#     if len(fibo_list)>n:
+#         return fibo_list[n]
+#     else:
+#         fibo_list.append(fibo(n-1)+fibo(n-2))
+#         return fibo_list[n]
+
+for _ in range(input_num-2):
+    fibo_list.append(fibo_list[-1]+fibo_list[-2])
+
+print(fibo_list[input_num])
