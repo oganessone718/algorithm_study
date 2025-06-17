f432385d6760ae03eb6576fe794dfea383773337
오가네손 <105881036+oganessone718@users.noreply.github.com>
2025-06-17 18:11:33 +0900

    [Gold IV] Title: 가르침, Time: 1672 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -0,0 +1,40 @@
+import sys
+from itertools import combinations
+
+n, k = map(int, sys.stdin.readline().rstrip().split())
+
+k -= 5
+
+if k < 0:
+  print(0)
+  exit()
+
+used_char_set = set()
+essential_char_set = {'a', 'n', 't', 'i', 'c'}
+word_list = []
+for _ in range(n):
+  word = set(sys.stdin.readline().rstrip())-essential_char_set
+  word_list.append(word)
+  used_char_set.update(word)
+
+if len(used_char_set) <= k:
+  print(n)
+  exit()
+
+char_combinations = combinations(used_char_set, k)
+
+max_global = 0
+
+for char_combination in char_combinations:
+  char_combination_set = set(char_combination)
+  max_local = 0
+  for word in word_list:
+    if (word <= char_combination_set):
+      max_local+=1
+  if max_local == n:
+    print(n)
+    exit()
+  if max_local > max_global:
+    max_global = max_local
+
+print(max_global)
